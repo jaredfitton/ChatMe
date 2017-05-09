4cfa180b108b60438d080f724fbde38ca1a669a1
jaredfitton <jaredfitton@gmail.com>
2017-05-09 08:06:28 -0700

    Added firebase to the project

---
+++ ChatMe/ChatMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatMe
+//
+//  Created by Jared Fitton on 5/5/17.
+//  Copyright © 2017 JaredFitton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
